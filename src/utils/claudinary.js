4b1e2d61dd813331bfb15607a99f1f16ae1c5b70
hariom-mishra <mhariom014@gmail.com>
2025-06-03 08:02:59 +0530

    file upload

---
+++ src/utils/claudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLAUDINARY_CLOUD_NAME,
+    api_key: process.env.CLAUDINARY_API_KEY,
+    api_secret: process.env.CLAUDINARY_API_SECRET
+});
+
+
+const uploadOnClaudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        //upload the file on claudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //success
+        console.log("file uploaded successfully", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);//remove locally saved temporary file as failed to upload
+        return null;
+    }
+}
+
+export {uploadOnClaudinary};
